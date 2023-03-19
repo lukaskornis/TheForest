0f7c2113eea5b6e48cd19dc08a1278fac6126259
Viktor Kornilov <39262485+ViktorKornilov@users.noreply.github.com>
2023-03-19 14:07:40 +0200

    Noise, Shadergraph, FPS controller

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -6,6 +6,8 @@ public class TerrainGenerator : MonoBehaviour
     public int size = 256;
     public int maxHeight = 64;
     public float mountainSize = 50;
+
+    public float islandSize = 2;
     
     public bool autoGenerate = true;
     private Terrain terrain;
@@ -33,10 +35,16 @@ public class TerrainGenerator : MonoBehaviour
         {
             for (int x = 0; x < size; x++)
             {
-
+                // distance from center
+                var center = new Vector2(size / 2, size / 2);
+                var distance = Vector2.Distance(center, new Vector2(x, y));
+                distance /= islandSize;
+                distance = math.remap(0, size, 1, 0, distance);
+                
                 //Generate the height
                 var pos = new float2(x, y) / mountainSize;
-                var height = noise.pnoise(pos, size);
+                var height = math.remap(-1,1,0,1,noise.pnoise(pos, size));
+                height *= distance;
                 
                 //Set the height
                 heightmap[x, y] = height;
