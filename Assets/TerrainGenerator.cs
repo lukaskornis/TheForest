8f448dca39f2a3215a6923f15231e2e6ce6dbe97
Viktor Kornilov <39262485+ViktorKornilov@users.noreply.github.com>
2023-03-23 19:13:19 +0200

    Generator layers

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -5,9 +5,19 @@ public class TerrainGenerator : MonoBehaviour
 {
     public int size = 256;
     public int maxHeight = 64;
+    
+    // sizes
     public float mountainSize = 50;
+    public float hillSize = 15;
+    public float bumpSize = 6;
+    
+    // height
+    public float mountainHeight = 1;
+    public float hillHeight = 0.3f;
+    public float bumpHeight = 0.02f;
 
     public float islandSize = 2;
+    
     
     public bool autoGenerate = true;
     private Terrain terrain;
@@ -42,10 +52,15 @@ public class TerrainGenerator : MonoBehaviour
                 distance = math.remap(0, size, 1, 0, distance);
                 
                 //Generate the height
-                var pos = new float2(x, y) / mountainSize;
-                var height = math.remap(-1,1,0,1,noise.pnoise(pos, size));
-                height *= distance;
+                float Noise(Vector2 pos)=> math.remap(-1,1,0,1,noise.pnoise(pos, size));
+                var pos = new float2(x, y);
                 
+                var height = Noise(pos / mountainSize) * mountainHeight;
+                height -=  Noise(pos / hillSize) * hillHeight;
+                height += Noise(pos / bumpSize) * bumpHeight;
+                
+                height *= distance;
+
                 //Set the height
                 heightmap[x, y] = height;
             }
